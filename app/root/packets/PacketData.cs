b6908220686864f07009bd24fb1ed21f83f3c5b4
gui castaldeli <guicastaldeli8@gmail.com>
2026-03-20 17:35:20 -0400

    data update

---
+++ app/root/packets/PacketData.cs
@@ -0,0 +1,25 @@
+namespace App.Root.Packets;
+
+class PacketData : Packet {
+    public Dictionary<DataType, List<Dictionary<string, object>>> data {
+        get;
+        set;
+    }
+
+    public PacketData() {
+        type = PacketType.DATA;
+    }
+
+    // Snapshot
+    public static PacketData fromSnapshot(DataSnapshot snapshot) {
+        return new PacketData {
+            data = snapshot.data
+        };
+    }
+
+    public DataSnapshot toSnapshot() {
+        return new DataSnapshot {
+            data = data
+        };
+    }
+}
--- app/root/packets/PacketType.cs
+++ app/root/packets/PacketType.cs
@@ -4,7 +4,6 @@ enum PacketType {
     JOIN,
     LEAVE,
     STATE,
-    WORLD,
-    WORLD_DATA,
-    PING
+    PING,
+    DATA
 }
