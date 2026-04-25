a8eca2dfe7845e6d5e32df67891f946a6d497f24
gui castaldeli <guicastaldeli8@gmail.com>
2026-04-24 22:32:28 -0400

    SkyboxTime and WorldParticleTime added

---
+++ app/root/world/env/particle/WorldParticleTime.cs
@@ -0,0 +1,27 @@
+/**
+    
+    Skybox Time class to help
+    set the time and colors.
+    
+    */
+namespace App.Root.World.Env.Particle;
+
+/**
+    
+    Particle Color helper
+    
+    */
+static class ParticleColor {
+    
+}
+
+/**
+    
+    Skybox Time class
+    
+    */
+class WorldParticleTime {
+    public WorldParticleTime() {
+        
+    }
+}
---
+++ app/root/world/env/skybox/SkyboxTime.cs
@@ -0,0 +1,27 @@
+/**
+    
+    Skybox Time class to help
+    set the time and colors.
+    
+    */
+namespace App.Root.World.Env.Skybox;
+
+/**
+    
+    Skybox Color helper
+    
+    */
+static class SkyboxColor {
+    
+}
+
+/**
+    
+    Skybox Time class
+    
+    */
+class SkyboxTime {
+    public SkyboxTime() {
+        
+    }
+}
