b2c0de8ba8d28afa68f171f8e42482ec1d11c919
gui castaldeli <guicastaldeli8@gmail.com>
2026-04-29 14:16:45 -0400

    Weather added

---
+++ app/root/world/weather/Weather.cs
@@ -0,0 +1,7 @@
+namespace App.Root.World.Weather;
+
+class Weather : WorldHandler {
+    public Weather() {
+        
+    }    
+}
---
+++ app/root/world/weather/WeatherActivator.cs
@@ -0,0 +1,7 @@
+namespace App.Root.World.Weather;
+
+class WeatherActivator {
+    public WeatherActivator() {
+        
+    }
+}
