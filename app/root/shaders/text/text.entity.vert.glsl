4f913c5fe580d031e90573d6b89ece2356dbbfed
gui castaldeli <guicastaldeli8@gmail.com>
2026-05-27 10:16:01 -0400

    Updateee

--- app/root/shaders/text/text.entity.vert.glsl
+++ app/root/shaders/text/text.entity.vert.glsl
@@ -1,6 +1,6 @@
 void setTextEntityVert() {
     vec3 camRight = vec3(uView[0][0], uView[1][0], uView[2][0]);
-    vec3 camUp = vec3(uView[0][1], uView[1][1], uView[2][1]);
+    vec3 worldUp = vec3(0.0, 1.0, 0.0);
 
     vec3 worldCenter = vec3(uModel[3][0], uModel[3][1], uModel[3][2]);
     float scaleX = length(vec3(uModel[0][0], uModel[0][1], uModel[0][2]));
@@ -8,7 +8,7 @@ void setTextEntityVert() {
 
     vec3 finalPos = worldCenter +
         camRight * aPos.x * scaleX +
-        camUp * aPos.y * scaleY;
+        worldUp * aPos.y * scaleY;
 
     gl_Position = uProjection * uView * vec4(finalPos, 1.0);
     vTexCoord = aTexCoord;
