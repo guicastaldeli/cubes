b4564f629bfd2ce09ebf9734989b643bb60e48a7
gui castaldeli <guicastaldeli8@gmail.com>
2026-05-27 21:38:21 -0400

    Shop added

---
+++ app/root/player/shop/Shop.cs
@@ -0,0 +1,9 @@
+namespace App.Root.Player.Shop;
+
+class Shop {
+    public const string ID = "shop";
+
+    public Shop() {
+        
+    }
+}
---
+++ app/root/player/shop/ShopUI.cs
@@ -0,0 +1,29 @@
+namespace App.Root.Player.Shop;
+using App.Root.UI;
+
+class ShopUI : UI {
+    public static string SHOP_DIR = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "player/shop/");
+    public static string PATH = SHOP_DIR + "shop.xml";
+    
+    public ShopUI() : base(PATH, Shop.ID) {
+        
+    }
+
+    /**
+    
+        Render
+    
+        */
+    public override void render() {
+        base.render();
+    }
+
+    /**
+    
+        Update
+    
+        */
+    public override void update() {
+        base.update();
+    }
+}
