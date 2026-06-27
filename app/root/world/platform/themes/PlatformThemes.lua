e2b6c5944f3c9d41530ab7c46e1211e814e0667c
gui castaldeli <guicastaldeli8@gmail.com>
2026-06-27 11:21:18 -0400

    Themes added

---
+++ app/root/world/platform/themes/PlatformThemes.lua
@@ -0,0 +1,53 @@
+--[[
+    
+    General Platform Themes
+
+    ]]
+dofile("CalculateMovement.lua")
+dofile("utils/ResolveFormat.lua")
+
+local texturePath = "texture/world/platform_themes/"
+
+-- Theme Payload
+local Theme = {}
+Theme.__index = Theme
+
+function Theme:new(params)
+    params = params or {}
+
+    local self = setmetatable({}, Theme)
+    self.id = params.id
+    self.name = params.name
+    self.movement = params.movement or ""
+    self.audio = params.audio or ""
+    self.texture = texturePath .. parms.texture
+
+    if params.custom then
+        for key, value in pairs(params.custom) do
+            self[key] = value
+        end
+    end
+
+    return self
+end
+
+-- Themes
+local Themes = {
+    Theme.new({
+        id = -1,
+        name = "TEST_1",
+        --movement = "",
+        --audio = "",
+        texture = "test1"
+    }),
+    Theme.new({
+        id = -2,
+        name = "TEST_2",
+        texture = "test2"
+    }),
+    Theme.new({
+        id = -3,
+        name = "TEST_3",
+        texture = "test3"
+    }),
+}
